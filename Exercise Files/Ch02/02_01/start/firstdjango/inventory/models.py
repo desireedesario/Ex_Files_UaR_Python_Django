f772f6a960e1b6e063c0a64912e8602374850404
Desi Desario <desireedesario@gmail.com>
2016-10-25 16:36:59 -0700

    file sturcture

---
+++ Exercise Files/Ch02/02_01/start/firstdjango/inventory/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+#The models.py file contains the set of models for the Django app.
+#A model is a class inheriting from django.db.models.Model, and is used to define fields as class attributes.
