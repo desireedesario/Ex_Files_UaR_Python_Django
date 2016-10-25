4210b339f0a7b7ff65ed1d8fd543ac62bd472962
Desi Desario <desireedesario@gmail.com>
2016-10-25 16:50:50 -0700

    creates inventory model Item

--- Exercise Files/Ch02/02_01/start/firstdjango/inventory/models.py
+++ Exercise Files/Ch02/02_01/start/firstdjango/inventory/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 #The models.py file contains the set of models for the Django app.
 #A model is a class inheriting from django.db.models.Model, and is used to define fields as class attributes.
+class Item(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    amount = models.IntegerField()
