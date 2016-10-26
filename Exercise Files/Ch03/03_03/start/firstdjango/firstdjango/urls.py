31d292a56aee196d769106033445580533b12877
Desi Desario <desireedesario@gmail.com>
2016-10-25 21:32:26 -0700

    views pages

--- Exercise Files/Ch03/03_03/start/firstdjango/firstdjango/urls.py
+++ Exercise Files/Ch03/03_03/start/firstdjango/firstdjango/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from inventory import views
+
 urlpatterns = [
+url(r'^$', views.index, name='index'),
+url(r'^item/(?P<id>\d+)/', views.item_detail, name='item_detail'),
+url(r'^admin/', include(admin.site.urls)),
     # Examples:
     # url(r'^$', 'firstdjango.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
 ]
--- Exercise Files/Ch03/03_03/start/firstdjango/inventory/views.py
+++ Exercise Files/Ch03/03_03/start/firstdjango/inventory/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse('<p>In Index View</p>')
+
+def item_detail(request, id):
+    return HttpResponse('<p>In Item detail view id {0}</p>'.format(id))
 # Create your views here.
