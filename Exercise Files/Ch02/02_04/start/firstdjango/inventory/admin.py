c909a713e385667d5d58a671f372161cea69344e
Desi Desario <desireedesario@gmail.com>
2016-10-25 20:47:33 -0700

    admin create Superuser

--- Exercise Files/Ch02/02_04/start/firstdjango/inventory/admin.py
+++ Exercise Files/Ch02/02_04/start/firstdjango/inventory/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Item
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'amount']
+
+admin.site.register(Item, ItemAdmin)
+
 # Register your models here.
